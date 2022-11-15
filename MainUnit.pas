1ce393ef1b66fb9a799d3a1cd5f99411c7539c7f
Aleksey Aleshkov <alf.aleshkov@gmail.com>
2022-11-15 11:55:27 +0300

    Add help with shortcut keys and few new functions
    + edge respawn mode
    + draw trail with right mouse button
    + kill weak agents mode
    + popup hint with parameters

--- MainUnit.pas
+++ MainUnit.pas
@@ -6,10 +6,10 @@ uses
   System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants,
   FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs, FMX.Effects,
   FMX.Controls.Presentation, FMX.StdCtrls, FMX.Objects,  FMX.Utils,
-  System.Math, System.Threading;
+  System.Math, System.DateUtils, System.Threading;
 
 const
-  NUM_OF_AGENTS = 65000;
+  NUM_OF_AGENTS = 25000;
 
 type
   TAntObj = Record
@@ -26,6 +26,9 @@ type
       Shift: TShiftState);
     procedure FormMouseDown(Sender: TObject; Button: TMouseButton;
       Shift: TShiftState; X, Y: Single);
+    procedure FormKeyDown(Sender: TObject; var Key: Word; var KeyChar: Char;
+      Shift: TShiftState);
+    procedure FormMouseMove(Sender: TObject; Shift: TShiftState; X, Y: Single);
   private
     { Private declarations }
   public
@@ -36,9 +39,19 @@ type
     alpha_param:byte;
     stillmove:boolean;
     mode:byte;
+    respawn_mode:Boolean;
+    TrailKiller:boolean;
+    KillWeak:boolean;
     settings_angle:Single;
+    ShowInstructions:Boolean;
+    InstructionsText:string;
+    Hint_StartTime:TDateTime;
+    Hint_Show:boolean;
+    Hint_Text:string;
     function Sense(Ant:TAntObj;AngleOffset:Single;Data: TBitmapData):Byte;
     procedure AgentMove(i:Integer;Data:TBitmapData;alpha_param:byte;w,h:Integer);
+    procedure ShowHint(hint:string);
+    procedure HintDraw(canvas:TCanvas);
   end;
 
 var
@@ -52,6 +65,11 @@ var
   Angle:Single;
   newPosX,newPosY:Single;
 begin
+if TrailKiller and (Ant.x>Ants[1].x-50) and (Ant.x<Ants[1].x+50) and (Ant.y>Ants[1].y-50) and (Ant.y<Ants[1].y+50)
+    and(ant.x <> Ants[1].x)and(ant.y <> Ants[1].y) then begin
+      result:=0;
+      Exit;
+      end;
   Angle:=Ant.direction + AngleOffset;
   newPosX:=Ant.x+sin(Angle)*(speed+2);
   newPosY:=Ant.y+cos(Angle)*(speed+2);
@@ -63,8 +81,6 @@ Result := TAlphaColorRec(Data.GetPixel(Round(newPosX),Round(newPosY))).R;
 //if Result>100 then Result:=Result-(Result-100)*2;
 if (Result<10) and (mode = 1) then Result:=50;
 if (Result>100) and (mode = 1) then Result:=0;
-if (Ant.x>Ants[1].x-50) and (Ant.x<Ants[1].x+50) and (Ant.y>Ants[1].y-50) and (Ant.y<Ants[1].y+50)
-                        and(ant.x <> Ants[1].x)and(ant.y <> Ants[1].y) then result:=0;
 if (Result>100) and (mode = 2) then Result:=Result-(Result-100)*2;
 //if (Result<10) and (mode = 4) then Result:=TAlphaColorRec(Data.GetPixel(Round(newPosX),Round(newPosY))).B;
 if mode = 3 then Result:=(Result*Random(100)) div 100;
@@ -136,11 +152,16 @@ weights[1]:=Sense(ants[i],settings_angle,Data);
 weights[2]:=Sense(ants[i],0,Data);
 weights[3]:=Sense(ants[i],-settings_angle,Data);
 r:=TAlphaColorRec(Data.GetPixel(Round(Ants[i].x),Round(Ants[i].y))).R;
+if KillWeak and (r = 0)then begin
+  Ants[i].x:=w div 2;
+  Ants[i].y:=h div 2;
+  r:=200;
+  end;
 r:=r+20;
 if r>200 then r:=200;
 Data.SetPixel(Round(Ants[i].x),Round(Ants[i].y),ARGBtoColorChannels(alpha_param,r,Ants[i].Gcolor,Ants[i].Gcolor div 2 + 128));
-if i = 1 then
-  Data.SetPixel(Round(Ants[i].x),Round(Ants[i].y),ARGBtoColorChannels(255,255,255,255));
+if TrailKiller and (i = 1) then
+  Data.SetPixel(Round(Ants[i].x),Round(Ants[i].y),$FFFFFFFF);
 
 if (weights[2]>weights[1]) and (weights[2]>weights[3]) then begin
   //Ants[i].direction:=Ants[i].direction;
@@ -163,13 +184,20 @@ if stillmove then begin
   Ants[i].y:=Ants[i].y+cos(Ants[i].direction)*speed;
   end;
 
-if Ants[i].x>w-1 then Ants[i].x:=0;
-if Ants[i].x<0 then Ants[i].x:=w-1;
-if Ants[i].y>h-1 then Ants[i].y:=0;
-if Ants[i].y<0 then Ants[i].y:=h-1;
+if respawn_mode then begin
+    if (Ants[i].x>w-1) or (Ants[i].x<0) or (Ants[i].y>h-1) or (Ants[i].y<0) then begin
+      Ants[i].x:=w div 2;
+      Ants[i].y:=h div 2;
+    end;
+  end else begin
+    if Ants[i].x>w-1 then Ants[i].x:=0;
+    if Ants[i].x<0 then Ants[i].x:=w-1;
+    if Ants[i].y>h-1 then Ants[i].y:=0;
+    if Ants[i].y<0 then Ants[i].y:=h-1;
+  end;
 //Data.SetPixel(Round(Ants[i].x),Round(Ants[i].y),ARGBtoColorChannels(alpha_param,r,Ants[i].Gcolor,255));
-if i = 1 then
-  Data.SetPixel(Round(Ants[i].x),Round(Ants[i].y),ARGBtoColorChannels(255,255,255,255));
+if TrailKiller and (i = 1) then
+  Data.SetPixel(Round(Ants[i].x),Round(Ants[i].y),$FFFFFFFF);
 end;
 
 procedure TForm1.Button1Click(Sender: TObject);
@@ -191,9 +219,13 @@ TParallel.&For(1,NUM_OF_AGENTS,
   begin
     AgentMove(i,Data,alpha_param,bmp.Width,bmp.Height);
   end);
+//if KillWeak then KillWeak:=false;
 bmp.Unmap(Data);
 Canvas.BeginScene();
 Canvas.DrawBitmap(bmp,RectF(0,0,bmp.Width,bmp.Height),RectF(0,0,Canvas.Width,Canvas.Height),1);
+if ShowInstructions then
+  Canvas.FillText(RectF(100,0,Canvas.Width,Canvas.Height), InstructionsText, false, 100, [],TTextAlign.Leading, TTextAlign.taCenter);
+HintDraw(Canvas);
 Canvas.EndScene;
 end;
 
@@ -211,6 +243,9 @@ settings_angle:=0.3;
 alpha_param:=180;
 stillmove:=true;
 mode:=0;
+respawn_mode:=true;
+TrailKiller:=false;
+KillWeak:=false;
 BaseBitmap:=TBitmap.Create(Canvas.Width ,Canvas.Height );
 for i := 1 to NUM_OF_AGENTS do begin
 //  Ants[i].x:=Random(BaseBitmap.Width);
@@ -223,33 +258,73 @@ for i := 1 to NUM_OF_AGENTS do begin
 BaseBitmap.Canvas.BeginScene;
 BaseBitmap.Canvas.Clear($FF000000);
 BaseBitmap.Canvas.EndScene;
+Canvas.Fill.Color := TAlphaColors.White;
+InstructionsText:='Physarum polycephalum simulation'#10#13 +
+  'by Aleshkov A.F., https://github.com/AlfAleshkov/Physarum-simulation'#10#13 +
+  'Shortcut keys:'#10#13 +
+  '"m" or "0","1","2"... - change mode of sensor calculation'#10#13 +
+  '"p" - Pause'#10#13 +
+  'Space - Agents pause, trail diffusion continues'#10#13 +
+  '"+","-" - change agent angle'#10#13 +
+  '"z","x" - change agent transparency (alpha channel)'#10#13 +
+  '"r" - respawn mode: when reaching edges - die and respawn at center'#10#13 +
+  '"k" - Enable/Disable trail-killer, one of agent (white point) turns off agents sensors around him'#10#13 +
+  '"e" - Kill weak lonely agents'#10#13 +
+  'Left click - Clear trail map within 50px radius'#10#13+
+  '"F1" - show/hide this instructions';
+ShowInstructions:=False;
+ShowHint('Press F1 to view shortcuts keys');
 end;
 
+
+procedure TForm1.FormKeyDown(Sender: TObject; var Key: Word; var KeyChar: Char;
+  Shift: TShiftState);
+begin
+if Key = vkF1 then ShowInstructions:= not ShowInstructions;
+end;
 
 procedure TForm1.FormKeyUp(Sender: TObject; var Key: Word; var KeyChar: Char;
   Shift: TShiftState);
 begin
-if KeyChar = '1' then begin
-  beep;
-  Timer1.Enabled:= not Timer1.Enabled;
-  end;
-if KeyChar = 'w' then
+if KeyChar = 'p' then Timer1.Enabled:= not Timer1.Enabled;
+if KeyChar = 'x' then
   alpha_param:=alpha_param+10;
-if KeyChar = 's' then
+if KeyChar = 'z' then
   alpha_param:=alpha_param-10;
 if KeyChar = ' ' then stillmove:=not stillmove;
 if KeyChar = 'm' then begin
   mode:=(mode+1) mod 4;
-  Caption:='Mode: '+IntToStr(mode);
+  ShowHint('Mode No: '+IntToStr(mode));
   end;
+if KeyChar in ['0','1','2','3','4'] then begin
+  mode:=StrToInt(KeyChar);
+  ShowHint('Mode No: '+IntToStr(mode));
+  end;
+if KeyChar = 'r' then begin
+  respawn_mode:= not respawn_mode;
+  if respawn_mode then
+    ShowHint('Respawn at center, ON') else ShowHint('Respawn mode OFF');
+  end;
+if KeyChar = 'k' then begin
+  TrailKiller:= not TrailKiller;
+  if TrailKiller then
+    ShowHint('Trail-killer start hunting') else ShowHint('Trail-killer disabled');
+  end;
+if KeyChar = 'e' then begin
+  KillWeak:=not KillWeak;
+  if KillWeak then
+    ShowHint('Weak agent execution') else ShowHint('Weak execution disabled');
+  end;
+
 if KeyChar = '-' then begin
   settings_angle:=settings_angle-0.02;
-  Caption:='settings_angle: '+FloatToStr(settings_angle);
+  ShowHint('settings_angle: '+IntToStr(Round(settings_angle * 180/pi)));
   end;
 if KeyChar = '+' then begin
   settings_angle:=settings_angle+0.02;
-  Caption:='settings_angle: '+FloatToStr(settings_angle);
+  ShowHint('settings_angle: '+IntToStr(Round(settings_angle * 180/pi)));
   end;
+
 end;
 
 procedure TForm1.FormMouseDown(Sender: TObject; Button: TMouseButton;
@@ -264,6 +339,36 @@ if Button = TMouseButton.mbLeft then begin
   BaseBitmap.Canvas.FillEllipse(RectF(dx-50,dy-50,dx+50,dy+50),1);
   BaseBitmap.Canvas.EndScene;
   end;
+end;
+
+procedure TForm1.FormMouseMove(Sender: TObject; Shift: TShiftState; X,
+  Y: Single);
+var dx,dy:Integer;
+begin
+if ssRight in Shift then begin
+  BaseBitmap.Canvas.BeginScene;
+  BaseBitmap.Canvas.Fill.Color:=$FFFF0000;
+  dx:=Round(BaseBitmap.Width*x/canvas.Width);
+  dy:=Round(BaseBitmap.Height*y/canvas.Height);
+  BaseBitmap.Canvas.FillEllipse(RectF(dx-10,dy-10,dx+10,dy+10),1);
+  BaseBitmap.Canvas.EndScene;
+  end;
+end;
+
+procedure TForm1.ShowHint(hint:string);
+begin
+Hint_StartTime:=Now;
+Hint_Show:=true;
+Hint_Text:=hint;
+end;
+
+procedure TForm1.HintDraw(canvas:TCanvas);
+begin
+if Hint_Show then begin
+  if SecondsBetween(Hint_StartTime, Now) < 2 then begin
+    Canvas.FillText(RectF(100,10,Canvas.Width,Canvas.Height), Hint_Text, false, 100,[],TTextAlign.Leading, TTextAlign.Leading);
+  end else  Hint_Show:=false;
+end;
 end;
 
 end.
